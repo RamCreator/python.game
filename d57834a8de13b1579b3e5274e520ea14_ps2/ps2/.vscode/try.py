7886839c440e7106ffaf0fd8fcecd16409295e7f
RamCreator <rammaye87540644@gmail.com>
2024-01-03 11:19:56 +0530

    new_01

---
+++ d57834a8de13b1579b3e5274e520ea14_ps2/ps2/.vscode/try.py
@@ -0,0 +1,20 @@
+num = '10'
+
+# # check and print type num variable
+# print(type(num))
+
+# # convert the num from string into int
+# converted_num = int(num)
+
+# # print type of converted_num
+# print(type(converted_num))
+
+# # We can check by doing some mathematical operations
+# print(converted_nu
+while True:
+    i = 0
+    for i in range(1,10):
+        print(i)
+        break
+    if(i)
+   
